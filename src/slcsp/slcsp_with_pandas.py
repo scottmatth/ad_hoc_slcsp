b8cc9a67539a5387c89dd5af1b8f85f3f7543174
Scott Matthews <scott.g.matthews@gmail.com>
2025-04-27 15:50:41 -0400

    Touching up pandas solution and adding some comments

--- src/slcsp/slcsp_with_pandas.py
+++ src/slcsp/slcsp_with_pandas.py
@@ -1,10 +1,28 @@
 import pandas as pd
 from slcsp import ROOT_SRC_DIR
 
-def determine_second_lowest_rate(zipcode:str, zip_to_rates:pd.DataFrame) -> int | None:
+def determine_second_lowest_rate(zipcode:str,
+                                 zip_to_rates:pd.DataFrame) -> int | None:
+    """
+    Parses the joined dataframe of existing zipcodes and rates to determine the
+    second lowest cost silver rate for the given Zipcode
+
+    Parameters:
+        zipcode (str): zip code against which this method should find the second lowest
+            rate (if) possible
+        zip_to_rates (DataFrame): A pandas dataframe which is an Inner Join of the content of
+            the provided plans.csv and the provided Zips.csv, joined by the State and Rate_area 
+    """
     result = None
+
+    # There must be more than one Rate to evaluate in order to find the second lowest rate
     if len(set(zip_to_rates.query(f"zipcode == @zipcode").rate.values)) > 1:
+        # If there are more than one index combinations of Zipcode to State to Rate_area,
+        # this is too ambiguous to determine
         if len(set(zip_to_rates.query(f"zipcode == @zipcode").index)) == 1:
+
+            # Determine the second lowest rate by sorting the set of values (in case 2 are the same)
+            # and getting the second one in the list
             result = sorted(list(map(float, set(zip_to_rates.query(f"zipcode == @zipcode").rate.values))))[1]
     return result
 
@@ -16,28 +34,19 @@ if __name__ == '__main__':
     all_zips = pd.read_csv(str(ROOT_SRC_DIR) + "/data_files/zips.csv",
                            index_col=["zipcode","state", "rate_area"],dtype=pd.StringDtype())
 
+    # Inner Join the plans and zips csv's by the state and rate area
     plans_for_zips = all_zips.join(all_plans[all_plans['metal_level'] == 'Silver'],
                                    on =["state", "rate_area"],
                                    lsuffix="_plans", rsuffix="zips",
                                    how="inner")
 
-    slcsp_source = pd.read_csv(str(ROOT_SRC_DIR) + "/data_files/slcsp.csv",dtype=pd.StringDtype())
+    # Get the initial slcsp values from the provided file, and print them out with any
+    # rate values we were able to assertain
+    slcsp_source = pd.read_csv(str(ROOT_SRC_DIR) + "/data_files/slcsp.csv",
+                               dtype=pd.StringDtype())
+    print(f"{slcsp_source.columns[0]},{slcsp_source.columns[1]}")
     for slcsp in slcsp_source.values:
         second_lowest = determine_second_lowest_rate(slcsp[0], plans_for_zips)
         output = slcsp[0] + "," + (str(second_lowest) if second_lowest is not None else "")
         print(output)
 
-    
-##    print(slcsp_source.values)
-##    control_zipcodes = slcsp_source. 
-    
-##    print(plans_for_zips.index)
-
-##    print(plans_for_zips.query("zipcode == 82605")["rate_area"].nunique())
-##    print(len(set(plans_for_zips.query("zipcode == 82605").index)))
-##    print(len(set(plans_for_zips.query("zipcode == 82605").rate.values)))
-
-##    just_36003 = plans_for_zips.query("zipcode==36003 and metal_level=='Silver'").head(100)
-##    just_silver = plans_for_zips.query("metal_level=='Silver'")
-##    just_silver = plans_for_zips.query()
-##    print(just_silver.head(20))
